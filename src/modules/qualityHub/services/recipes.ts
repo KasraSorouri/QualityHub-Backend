0f2ecfb0f928764ea7c164375caab156d6bbc925
Kasra Sorouri <kasra.sorouri@gmail.com>
2024-04-01 22:09:13 +0300

    Add Rework

--- src/modules/qualityHub/services/recipes.ts
+++ src/modules/qualityHub/services/recipes.ts
@@ -24,7 +24,7 @@ const query: RecipeQuery = {
         {
           model: Material,
           as: 'material',
-          attributes: ['id', 'itemShortName', 'itemLongName', 'itemCode', 'price', 'unit', 'active'],
+          attributes: ['id', 'itemShortName', 'itemLongName', 'itemCode', 'price', 'unit', 'traceable', 'active'],
         },
       ],
     }
